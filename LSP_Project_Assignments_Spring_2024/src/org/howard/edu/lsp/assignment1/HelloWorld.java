9930e32d2018ed7dfb6ea3bb9c9a7f1dbe4c5d2c
Rijan Khatri <rijan.khatri@bison.howard.edu>
2024-04-02 00:42:11 +0545

    Assignment #1

---
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+*  Name: Rijan Khatri
+*/
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
